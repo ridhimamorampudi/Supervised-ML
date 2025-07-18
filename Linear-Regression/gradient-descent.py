58572096a2d520fbc0fe7bc135543f4b4f53c11c
Ridhima Morampudi <ridhima.morampudi@gmail.com>
2025-07-17 21:25:46 -0700

    linear regression - gradient descent

---
+++ Linear-Regression/gradient-descent.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate fake data
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Add bias term
+X_b = np.c_[np.ones((100, 1)), X]
+
+# Gradient Descent Setup
+eta = 0.1
+n_iterations = 1000
+m = 100
+theta = np.random.randn(2, 1)
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+
+print("Estimated theta:", theta)
+
+# Plot
+plt.plot(X, y, "b.")
+plt.plot(X, X_b.dot(theta), "r-")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Linear Regression via Gradient Descent")
+plt.show()
